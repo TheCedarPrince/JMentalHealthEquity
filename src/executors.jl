50b2c40b5e934eb8ee256da37838236886749b4a
TheCedarPrince <jacobszelko@gmail.com>
2022-04-01 00:39:56 -0400

    Changed purpose and moved to generators

--- src/executors.jl
+++ src/executors.jl
@@ -8,31 +8,8 @@ function ExecuteAudit(data::DataFrame; hitech = true)
 
 end
 
-function ExecuteConnection(;
-    host = nothing,
-    port = nothing,
-    user = nothing,
-    password = nothing,
-    dialect = :postgresql,
-    schema = nothing,
-)
-
-    if dialect == :postgresql
-        conn = DBInterface.connect(
-            LibPQ.Connection,
-            "host=$host port=$port user=$user password=$password",
-        )
-        db_info = reflect(conn; schema = schema, dialect = dialect)
-
-        for key in keys(db_info.tables)
-            @eval global $(Symbol(key)) = $(db_info[key])
-        end
-
-        return conn
-    end
-
-end
-
 # ASSUMPTION: The Database follows the OMOP CDM 5.4 Schema completely
 
-export ExecuteAudit, ExecuteConnection
+export ExecuteAudit
+
+
